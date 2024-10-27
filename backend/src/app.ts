35f1a54a3f8a64f91c9a97b3904d49d3e3ca8ae5
ronalt4cs <ronaltaugustocs@gmail.com>
2024-10-27 19:11:56 -0300

    feat: configure cors to frontend app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,15 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
 import { errorHandler } from './http/middlewares/error-handler'
+import cors from '@fastify/cors'
+import { env } from './env'
 
 export const app = fastify()
+
+app.register(cors, {
+  origin: '*',
+  methods: ['GET']
+})
 
 app.register(appRoutes)
 
